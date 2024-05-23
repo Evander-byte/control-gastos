6c7b6cb0a1f3e898158e2664cec6f49385e0a2ee
Evander-byte <emondragon571@gmail.com>
2024-05-23 15:43:37 -0600

    Campo calendario y guardando todo en el state

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+type ErrorMessageProps = {
+    children: ReactNode
+}
+
+const ErrorMessage = ({ children }: ErrorMessageProps) => {
+    return (
+        <p className="bg-red-600 p-2 text-white font-bold text-center text-sm">
+            {children}
+        </p>
+    )
+}
+
+export default ErrorMessage
